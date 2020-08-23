9e04928cb76fb14dfda2ea27d01b7bb0b153f96e
chicago <balogun849@ymail.com>
2020-08-23 01:46:25 +0100

    it's all about mileage

--- automation-tutorial/src/test/java/com/automation/tutorial/SignInTest.java
+++ automation-tutorial/src/test/java/com/automation/tutorial/SignInTest.java
@@ -8,9 +8,18 @@ public class SignInTest {
 
 
     @Test
+<<<<<<< Updated upstream
     public void can_create_account_successfully(){
         Account bizAccount = CreateAccountForm.create("a@a.com");
         Assert.assertNotNull(bizAccount);
         Assert.assertEquals(bizAccount.getAddress(), "My Address");
+
     }
+=======
+    
+public void (){
+   i love coding.
+ }
+
+>>>>>>> Stashed changes
 }
