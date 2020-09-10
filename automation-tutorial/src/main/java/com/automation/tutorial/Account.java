826beecdea19d6f974bccb2aa01591e1146718c0
chicago <balogun849@ymail.com>
2020-09-10 01:34:01 +0100

    white house

--- automation-tutorial/src/main/java/com/automation/tutorial/Account.java
+++ automation-tutorial/src/main/java/com/automation/tutorial/Account.java
@@ -2,9 +2,16 @@ package com.automation.tutorial;
 
 public interface Account {
 
+<<<<<<< Updated upstream
 */ 
 * GO
 * GO NOW
 * GO GO NOW
 *//
+=======
+*/
+* LIFE IS GOOD
+**/
+    public String getAddress();
+>>>>>>> Stashed changes
 }
